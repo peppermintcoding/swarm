dedadf37ce25fbe88c02cb62d9cf367a734b1ee7
Tim Betz <tim.betz@hs-augsburg.de>
2022-11-19 12:29:47 +0100

    added random movement

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use glutin_window::GlutinWindow;
 use graphics::{Context, Rectangle, Graphics};
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
-use piston::{RenderEvent, UpdateEvent, MouseCursorEvent, WindowSettings, UpdateArgs};
+use piston::{RenderEvent, MouseCursorEvent, WindowSettings};
 use vecmath::{vec2_add, vec2_normalized, vec2_scale, vec2_sub};
+use rand::Rng;
 
 
 struct App {
@@ -26,10 +27,13 @@ impl App {
 
     fn update(&mut self) {
         // let speed = 1.0 * args.dt;
-        let speed = 1.0;
+        let mut rng = rand::thread_rng();
+        let speed = 1.5;
+        let rand_speed = 1.1;
         for bird in &mut self.birds {
+            let rand_direction = vec2_scale(vec2_normalized([rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0)]), rand_speed);
             let direction = vec2_scale(vec2_normalized(vec2_sub(self.mouse, *bird)), speed);
-            *bird = vec2_add(*bird, direction);
+            *bird = vec2_add(*bird, vec2_add(direction, rand_direction));
         }
     }
 
@@ -52,7 +56,7 @@ fn main() {
     let mut app = App {mouse: [0.0; 2], birds: Vec::new()};
     app.birds.push([50.0, 50.0]);
     app.birds.push([250.0, 250.0]);
-    app.birds.push([0.0, 0.0]);
+    app.birds.push([250.0, 50.0]);
 
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
